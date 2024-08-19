e990064019091053c6fc347254e4ba4f3dd65a7d
28Sandeep07 <kau4rsandeep34@gmail.com>
2024-08-19 15:53:54 +0200

    Move Doxygen documentation to docs/ for GitHub Pages

---
+++ docs/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['doxygen_5fexample_2epy_0',['doxygen_example.py',['../doxygen__example_8py.html',1,'']]]
+];
---
+++ docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['add_0',['add',['../doxygen__example_8py.html#a5257f7feff41da5f6c0ede4500880f77',1,'doxygen_example']]]
+];
