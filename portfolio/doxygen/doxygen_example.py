c2053bb389b33278c6283c4a1eb165c3137e7971
28Sandeep07 <kau4rsandeep34@gmail.com>
2024-08-19 12:42:25 +0200

    Add Doxygen configuration and documentation files

---
+++ portfolio/doxygen/doxygen_example.py
@@ -0,0 +1,13 @@
+## \file doxygen_example.py
+#  \brief Example Python file for Doxygen.
+#
+#  Detailed description of the file.
+
+## \fn add(a, b)
+#  \brief Adds two numbers.
+#  \param a First number.
+#  \param b Second number.
+#  \return Sum of a and b.
+
+def add(a, b):
+    return a + b
